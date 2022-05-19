59653df732600bd1eb184ca56d6f87732d443f21
jonasa64 <hvidovrefifa@gmail.com>
2022-05-19 22:06:18 +0200

    User controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+
+
+    public function index()
+    {
+    }
+
+    public function update(User $user, Request $request)
+    {
+    }
+
+    public function destroy(User $user)
+    {
+    }
+
+    public function show(User $user)
+    {
+    }
+}
