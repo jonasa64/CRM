f38194ef5a285145c1c3e5590c8d2d81104159b4
jonasa64 <hvidovrefifa@gmail.com>
2022-05-27 15:33:00 +0200

    Added logic for updating a user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,19 +14,32 @@ class UserController extends Controller
         // Find all users
         $users = User::all();
 
-        return Response()->json(["users" => $users], 200);
+        return Response()->json(["data" => $users], 200);
     }
 
     public function update(User $user, Request $request)
     {
+        // Update user
+        $isUpdated = $user->update([
+            'firstName' => $request['firstName'],
+            'lastName' => $request['lastName'],
+            'email' => $request['email'],
+        ]);
+
+        // Check that user was updated successfully
+        if ($isUpdated)
+            return Response()->json(["data" => "user updated"], 200);
+        else
+            return Response()->json(["data" => "Failed to update user"], 500);
     }
 
     public function destroy(User $user)
     {
+        // Check that user was deleted successfully
         if ($user->delete())
-            return Response()->json("User delete", 200);
+            return Response()->json(["data" => "User delete"], 200);
         else
-            return Response()->json("Failed to delete user", 500);
+            return Response()->json(["data" => "Failed to delete user"], 500);
     }
 
     public function show(User $user)
