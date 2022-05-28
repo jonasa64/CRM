4f535b372a4619c8f8d4ad65077af308cf48e366
jonasa64 <hvidovrefifa@gmail.com>
2022-05-28 19:29:57 +0200

    Api routes prospects

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProspectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -19,6 +20,8 @@ Route::get("/users", [UserController::class, "index"]);
 Route::get("/users/{user}", [UserController::class, "show"]);
 Route::put("/users/{user}", [UserController::class, "update"]);
 Route::delete("/users/{user}", [UserController::class, "destroy"]);
+
+Route::apiResource("prospects", [ProspectController::class]);
 
 
 
