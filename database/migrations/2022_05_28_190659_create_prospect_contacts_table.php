84591e08c6269b2da441da99b2088bc0c794c536
jonasa64 <hvidovrefifa@gmail.com>
2022-08-13 23:20:21 +0200

    Updates to migrations files

--- database/migrations/2022_05_28_190659_create_prospect_contacts_table.php
+++ database/migrations/2022_05_28_190659_create_prospect_contacts_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('prospect_contacts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('prospect_id');
+            $table->string('phone')->nullable();
+            $table->string('phone_mobil')->nullable();
+            $table->string('fax')->nullable();
+            $table->string('address')->nullable();
+            $table->string('address_2')->nullable();
+            $table->string('city')->nullable();
+            $table->string('province')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('country')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
