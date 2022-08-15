881fce90d9a73957a566332566f94b992c7f2604
jonasa64 <hvidovrefifa@gmail.com>
2022-08-15 06:48:35 +0200

    Update to use Requests in controller

--- app/Http/Controllers/ProspectContactController.php
+++ app/Http/Controllers/ProspectContactController.php
@@ -2,21 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactStoreRequest;
+use App\Http\Requests\ContactUpdateRequest;
 use App\Models\ProspectContact;
 use App\Models\Prospect;
 
 class ProspectContactController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -24,23 +17,14 @@ class ProspectContactController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Prospect $prospect)
+    public function store(ContactStoreRequest $request, Prospect $prospect)
     {
         ProspectContact::updateOrCreate(['prospect_id' => $prospect->id], $request->validated());
 
         return Response()->json(['data' => 'New contact successfully created']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+
 
     /**
      * Update the specified resource in storage.
@@ -49,21 +33,10 @@ class ProspectContactController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Prospect $prospect)
+    public function update(ContactUpdateRequest $request, Prospect $prospect)
     {
         $prospect->update($request->validated());
 
         return Response()->json(['data' => 'prospect contact successfully updated']);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
